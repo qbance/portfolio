f81bc02d0c7d7ba897e1b5539e05c2755fe74d7d
qbance <bance.quentin@yahoo.fr>
2026-08-10 10:29:59 +0200

    Update navbar.js
    Modification d'un commentaire

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -1,6 +1,6 @@
 async function chargerNavbar() {
     const response = await fetch('navbar.html');
-    const html = await response.text(); // .text() au lieu de .json(), puisque c'est du HTML brut
+    const html = await response.text(); 
     document.getElementById('navbar-container').innerHTML = html;
 }
 chargerNavbar();
