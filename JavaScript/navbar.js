3f0f5cabdca7be00ca3b9d2af95b7ab111aa7241
qbance <bance.quentin@yahoo.fr>
2026-08-10 10:34:58 +0200

    Update navbar.js
    modification du chemin de fichier.

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -1,5 +1,5 @@
 async function chargerNavbar() {
-    const response = await fetch('navbar.html');
+    const response = await fetch('HTML/navbar.html');
     const html = await response.text(); 
     document.getElementById('navbar-container').innerHTML = html;
 }
